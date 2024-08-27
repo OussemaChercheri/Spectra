5f68414d9e66a3f820df225e7f9f8d1295dd0c37
OussemaChercheri <oussama.chercheri@tek-up.de>
2024-08-27 22:24:50 +0100

    add authentification

--- Web/backend/mailtrap/emails.js
+++ Web/backend/mailtrap/emails.js
@@ -1,5 +1,5 @@
 import { mailtrapClient, sender } from './mailtrap.config.js';
-import { VERIFICATION_EMAIL_TEMPLATE } from './emailTemplate.js';
+import { PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from './emailTemplate.js';
 
 export const sendVerificationEmail = async (email, verificationToken) => {
 	const recipient = [{ email }];
@@ -21,7 +21,7 @@ export const sendVerificationEmail = async (email, verificationToken) => {
 	}
 };
 
-export const sendWelcomeEmail = async (email, name) => {
+export const sendWelcomeEmail = async (email, name, next) => {
     const recipient = [{ email }];
 
     try {
@@ -38,6 +38,45 @@ export const sendWelcomeEmail = async (email, name) => {
 
         console.log("Welcome email sent successfully", response);
     } catch (error) {
-        res.status(500).json({ success: false, message: "Server error" });
+        next(error);
     }
+};
+
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: "Reset your password",
+			html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+			category: "Password Reset",
+		});
+	} catch (error) {
+		console.error(`Error sending password reset email`, error);
+
+		throw new Error(`Error sending password reset email: ${error}`);
+	}
+};
+
+export const sendResetSuccessEmail = async (email) => {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: "Password Reset Successful",
+			html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+			category: "Password Reset",
+		});
+
+		console.log("Password reset email sent successfully", response);
+	} catch (error) {
+		console.log(`Error sending password reset success email`, error);
+
+		throw new Error(`Error sending password reset success email: ${error}`);
+	}
 }

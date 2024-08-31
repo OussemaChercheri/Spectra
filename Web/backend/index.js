9afb38ab5372ce128fa7a26e6c4ffe206c9dd789
OussemaChercheri <oussama.chercheri@tek-up.de>
2024-08-31 10:20:28 +0100

    add Frontend authentification

--- Web/backend/index.js
+++ Web/backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 import { errorHandler } from './middleware/errorHandler.js';
@@ -10,6 +11,9 @@ import authRoutes from './routes/auth.route.js';
 const app = express();
 dotenv.config();
 app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true}));
+
 app.use(cookieParser());
 
 const PORT= process.env.PORT || 3000;
